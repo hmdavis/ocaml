ee70f100da4ae11734c107b17320971a16b0b4ed
Harry <harrisonmaxwelldavis@gmail.com>
2013-06-27 00:33:19 -0400

    added type

--- circular_lists.ml
+++ circular_lists.ml
@@ -1,20 +1,22 @@
+type mlist = Mnil | Mcons of (int ref * mlist ref)
+
 let fold_left f acc l = 
   let rec help f acc lst = 
-  	match lst with 
-  	|Mnil -> failwith "whoops"
-  	|Mcons(a,b) -> 
-  		let temp = f acc !a in 
-  		if !b == l then temp 
-  		else help f temp !b in 
+    match lst with 
+    |Mnil -> failwith "whoops"
+    |Mcons(a,b) -> 
+      let temp = f acc !a in 
+      if !b == l then temp 
+      else help f temp !b in 
   help f acc l
 
 let map f lst = 
   let rec help l = 
-  		match l with 
-      |Mnil -> failwith "whoops"
-      |Mcons(a,b) ->
-        a:=(f !a); 
-        if (!b == lst) then () else help !b in 
+    match l with 
+    |Mnil -> failwith "whoops"
+    |Mcons(a,b) ->
+      a:=(f !a); 
+      if (!b == lst) then () else help !b in 
   help lst; lst 
 
 let length lst = 
